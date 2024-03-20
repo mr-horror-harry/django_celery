79fe41a058afecc372f4c25a1b2adc39c98f5d04
M A Harish J <mr.horror3737@gmail.com>
2024-03-20 12:43:46 +0000

    celery beat worked

--- celery_django_project/celery_tasks/models.py
+++ celery_django_project/celery_tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Student_data(models.Model):
+    regno = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    marks = models.FloatField()
+    gender = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.name 
--- celery_django_project/celery_tasks/tasks.py
+++ celery_django_project/celery_tasks/tasks.py
@@ -1,16 +1,23 @@
 from celery import shared_task
+# from celery_tasks.models import Student_data
 
 @shared_task(bind=True)
 def add_data(self, x, y):
     print(f"{x} + {y} = {x+y}")
+    # instance1 = Student_data(regno='A1234', name='John Doe', marks=85.5, gender='M')
+    # instance1.save()
     return f"Success...{x+y}"
 
 @shared_task(bind=True)
 def sub_data(self, x, y):
     print(f"{x} - {y} = {x-y}")
+    # instance2 = Student_data(regno='B5678', name='Jane Smith', marks=75.0, gender='F')
+    # instance2.save()
     return f"Success...{x-y}"
 
 @shared_task(bind=True)
 def mul_data(self, x, y):
     print(f"{x} * {y} = {x*y}")
+    # instance3 = Student_data(regno='C91011', name='Alice Johnson', marks=92.3, gender='F')
+    # instance3.save()
     return f"Success...{x*y}"
